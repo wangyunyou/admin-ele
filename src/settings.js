332b4cb01c1701964aa2b1f16954aae80e202dd6
wangyunyou <wangyunyou1995@163.com>
2020-01-20 10:12:31 +0800

    init

---
+++ src/settings.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+  /**
+   * 标题
+   */
+  title: '后台管理系统',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   * 固定头部
+   */
+  fixedHeader: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   * 显示logo
+   */
+  sidebarLogo: true
+}
